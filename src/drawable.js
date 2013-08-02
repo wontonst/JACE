9d14b0f1784e847d1e197bd684fe31620fdac554
Roy Zheng <roycraft3@gmail.com>
2013-08-02 11:04:00 -0700

    astyle & docs

--- src/drawable.js
+++ src/drawable.js
@@ -1,7 +1,15 @@
-
+/**
+ * @brief an object to be drawn onto the canvas
+ * @param {int} initx initial starting position of the object
+ * @param {int} inity initial starting position of the object
+ * @returns {Drawable}
+ */
 function Drawable(initx, inity) {
     this.position = new Coordinates(initx, inity);
 }
+/**
+ * @brief called each tick of the engine
+ */
 Drawable.prototype.tick = function() {
     console.log("ERROR: CALLING Drawable.tick WITHOUT USING INHERITANCE");
 };
--- src/frame.js
+++ src/frame.js
@@ -1,7 +1,7 @@
 /**
-@param {AtlasImage} img
-@param {int} pause milliseconds to display the frame
-*/
+ @param {AtlasImage} img
+ @param {int} pause milliseconds to display the frame
+ */
 function Frame(img, pause) {
     this.img = img;
     this.pause = Math.round(pause / engine.tickrate);
